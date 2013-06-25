a4857f9c063f3cfe027c9d71e1d7c7b5311ea9e2
Ryan Arthur <ryan.arthur5819@gmail.com>
2013-06-25 10:12:55 -0400

    finally digging into the api

--- js/weather.js
+++ js/weather.js
@@ -1,14 +1,35 @@
 $(document).ready(function() {
     $.ajax({
-        url : "http://api.wunderground.com/api/b4cb03d2871dd142/geolookup/conditions/forecast/q/OH/Cincinnati.json",
+        url : "http://api.wunderground.com/api/b4cb03d2871dd142/geolookup/conditions/q/OH/Cincinnati.json",
         dataType : "jsonp",
         success : function(parsed_json) {
         var location = parsed_json['location']['city'];
         var temp_f = parsed_json['current_observation']['temp_f'];
         var condition = parsed_json['current_observation']['weather']
+        var icon = parsed_json['current_observation']['icon_url']
         // alert("Current temperature in " + location + " is: " + temp_f + "°");
 
-        document.getElementById('current-temp').innerHTML=temp_f + "°" + "and" + " " + condition;
+        document.getElementById('current-temp').innerHTML=temp_f + "°";
+        document.getElementById('current-condition').innerHTML=condition;
+        document.getElementById('icon').src=icon;
+        }
+    });
+});
+
+
+$(document).ready(function() {
+    $.ajax({
+        url : "http://api.wunderground.com/api/b4cb03d2871dd142/geolookup/forecast/q/OH/Cincinnati.json",
+        dataType : "jsonp",
+        success : function(parsed_json) {
+
+            var location = parsed_json['location']['city'];
+            html = ""
+            var forecasttxt = parsed_json['forecast']['txt_forecast']['forecastday'].forEach(function(forecastday){
+                html = html + forecastday.title
+            });
+
+            document.getElementById('foretxt').innerHTML=html;
         }
     });
 });
